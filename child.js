f2a8d4439ca877e8b058639ebb87b9477049bbe7
Rifat-Earth <rifathasan01915@gmail.com>
2025-03-11 03:15:46 +0600

    yeah

--- child.js
+++ child.js
@@ -1,4 +1,7 @@
-document.getElementById("child")
-   .addEventListener("click",function(){
-    console.log('btn clicked')
+ const colors =['green','purple','blue','yellow','orange']
+ let index = 0;
+ document.getElementById("bgcolor")
+.addEventListener("click",function(){
+   document.body.style.backgroundColor =colors[index]; 
+   index=(index + 1)%colors.length;
 });
